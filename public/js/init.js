42c039c3752dd3acc7b86d3b0306f8267e6b616f
G00303381 <danielmaloneygti@gmail.com>
2017-05-11 10:41:47 +0100

    Added file structure

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //Activate the mobile side nav
+    $(".button-collapse").sideNav();
+
+    //Initialise the parallax
+    $('.parallax').parallax();
+    
+    //Initialise the carousel
+    $('.carousel').carousel();
+});
